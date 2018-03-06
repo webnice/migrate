b0ac90450952dafaf54afece57e384e18e5fc081
Alex Geer <monoflash@gmail.com>
2018-03-07 01:07:48 +0300

    Updated descriptions

--- migrate/main.go
+++ migrate/main.go
@@ -7,7 +7,7 @@ import (
 
 	"gopkg.in/webnice/migrate.v1/goose"
 
-	// Init DB drivers.
+	// Init database drivers
 	_ "github.com/go-sql-driver/mysql"  // Mysql
 	_ "github.com/kshvakov/clickhouse"  // Clickhouse
 	_ "github.com/lib/pq"               // Postgres, Cockroach, Redshift
@@ -22,14 +22,14 @@ func main() {
 	var db *sql.DB
 
 	cmd, arg = args()
-	// Create
+	// Exclusive execution of the create command
 	if cmd == `create` {
 		if err = goose.Run(cmd, nil, arg.Directory, arg.CreateName, arg.CreateType); err != nil {
 			log.Fatalf("Error: %s", err)
 		}
 		return
 	}
-	// Check driver
+	// Checking driver and set dialect
 	switch arg.Driver {
 	case `mysql`, `postgres`, `sqlite3`, `clickhouse`:
 		err = goose.SetDialect(arg.Driver)
@@ -44,14 +44,12 @@ func main() {
 	}
 	if err != nil {
 		log.Fatalf("%s\n", err)
-		return
 	}
-	// Open connection
+	// Opening database connection
 	if db, err = sql.Open(arg.Driver, arg.Dsn); err != nil {
 		log.Fatalf("Connect to database error: %s", err)
-		return
 	}
-	// Run
+	// Running command of database migration with different arguments
 	switch cmd {
 	case `up-to`, `down-to`:
 		err = goose.Run(cmd, db, arg.Directory, arg.UpDownVersion)
